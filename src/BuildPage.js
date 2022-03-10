89872d339dd6bde1a09fde324083e30ce0851a85
ThunderKlappe <timschley@gmail.com>
2022-03-09 17:57:16 -0800

    finished new game modal and updated main screen

--- src/BuildPage.js
+++ src/BuildPage.js
@@ -1,8 +1,9 @@
 import { DOMManip } from "./DOMManip";
+import { Modal } from "./Modal";
 
 export const BuildPage = (() => {
-    const _makeGrid = () => {
-        const gridContainer = DOMManip.makeNewElement("div", "", "board");
+    const _makeGrid = id => {
+        const gridContainer = DOMManip.makeNewElement("div", id, "board");
         for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 gridContainer.appendChild(
@@ -22,10 +23,16 @@ export const BuildPage = (() => {
     const buildStartingPage = () => {
         const header = DOMManip.makeNewElement("div", "header", "", "Battleship");
         const content = DOMManip.makeNewElement("div", "content");
-        const boardContainer = DOMManip.makeNewElement("div", "board-container");
+        const gameContainer = DOMManip.makeNewElement("div", "game-container");
+        const instructions = DOMManip.makeNewElement(
+            "div",
+            "game-instructions",
+            "board-label",
+            "The game is simple: destroy your opponent before they destroy you."
+        );
+        const boardsContainer = DOMManip.makeNewElement("div", "boards-container");
         const playerBoardWrapper = DOMManip.makeNewElement("div", "player-board-wrapper", "board-wrapper");
-        const playerBoard = _makeGrid();
-        playerBoard.setAttribute("id", "player-board");
+        const playerBoard = _makeGrid("player-board");
         const playerBoardLabel = DOMManip.makeNewElement(
             "div",
             "player-board-label",
@@ -39,8 +46,7 @@ export const BuildPage = (() => {
             "computer-board-wrapper",
             "board-wrapper"
         );
-        const computerBoard = _makeGrid();
-        computerBoard.setAttribute("id", "computer-board");
+        const computerBoard = _makeGrid("computer-board");
         const computerBoardLabel = DOMManip.makeNewElement(
             "div",
             "computer-board-label",
@@ -48,11 +54,34 @@ export const BuildPage = (() => {
             "Computer's Board"
         );
         DOMManip.appendChildren(computerBoardWrapper, computerBoard, computerBoardLabel);
+        const newGameButton = DOMManip.makeNewElement("button", "new-game-button", "page-button", "New Game");
 
-        DOMManip.appendChildren(boardContainer, playerBoardWrapper, computerBoardWrapper);
-        content.appendChild(boardContainer);
+        DOMManip.appendChildren(boardsContainer, playerBoardWrapper, computerBoardWrapper);
+
+        DOMManip.appendChildren(gameContainer, instructions, boardsContainer, newGameButton);
+        content.appendChild(gameContainer);
         DOMManip.appendChildren(document.body, header, content);
     };
 
-    return { buildStartingPage };
+    const buildNewGameModal = () => {
+        const newGameModal = DOMManip.makeNewElement("div", "new-game-modal", "modal content");
+        const newGameTitle = DOMManip.makeNewElement(
+            "div",
+            "new-game-title",
+            "modal-title",
+            "Please place your ships on the grid"
+        );
+        const shipName = DOMManip.makeNewElement("div", "ship-name", "ship-name");
+        const rotateButton = DOMManip.makeNewElement(
+            "button",
+            "ship-rotate-button",
+            "modal-button",
+            "Rotate"
+        );
+        const setUpGrid = _makeGrid("set-up-board");
+        DOMManip.appendChildren(newGameModal, newGameTitle, shipName, rotateButton, setUpGrid);
+        Promise.resolve(Modal.displayModal(newGameModal));
+    };
+
+    return { buildStartingPage, buildNewGameModal };
 })();
